1acb8c3bd6902666f51194c024b8fd79fcfb42fc
Lenna Peterson <lxpeters@purdue.edu>
2012-03-11 17:03:33 -0400

    Lexer gathers values, added parse precedence.

--- Bio/PDB/mmCIF/CIFyacc.py
+++ Bio/PDB/mmCIF/CIFyacc.py
@@ -20,18 +20,10 @@ def p_data_pairs_empty(p):
     """data_pairs : empty"""
     p[0] = []
 
-def p_data_pair(p):
-    """data_pair : TAG value"""
-    p[0] = (p[1], p[2])
-    
-def p_loop(p):
-    """data_pair : loop_header values"""
-    p[0] = p[1] + (p[2],)
+def p_data_header(p):
+    """data_pair : DATA"""
+    p[0] = ("data", p[1])
 
-def p_loop_header(p):
-    """loop_header : LOOP tags"""
-    p[0] = (p[1],) + (p[2],)
-    
 def p_tags_tag(p):
     """tags : TAG tags"""
     p[0] = (p[1],) + tuple(p[2])
@@ -41,12 +33,24 @@ def p_tags_empty(p):
     p[0] = []
 
 def p_values_value(p):
-    """values : value values"""
+    """values : VALUE values"""
     p[0] = (p[1],) + tuple(p[2])
 
 def p_values_empty(p):
     """values : empty"""
     p[0] = []
+    
+def p_loop(p):
+    """data_pair : loop_header values"""
+    p[0] = p[1] + (p[2],)
+
+def p_data_pair(p):
+    """data_pair : TAG VALUE"""
+    p[0] = (p[1], p[2])
+    
+def p_loop_header(p):
+    """loop_header : LOOP tags"""
+    p[0] = (p[1],) + (p[2],)
     
 def p_empty(p):
     """empty :"""
@@ -57,34 +61,34 @@ def p_empty(p):
     #p[0] = p[1]
 
 #<Value>  :     { '.' | '?' | <Numeric> | <CharString> | <TextField> }
-def p_value(p):
-    """value : number
-             | char_string
-             | text_field
-             | UNKNOWN
-             | INAPPLICABLE"""
-    p[0] = p[1]
+#def p_value(p):
+    #"""value : number
+             #| char_string
+             #| text_field
+             #| UNKNOWN
+             #| INAPPLICABLE"""
+    #p[0] = p[1]
 
 
 #<CharString>  :    <UnquotedString> | <SingleQuotedString> | <DoubleQuotedString>
-def p_char_string(p):
-    """char_string : EOL_UNQUOTED_STRING
-                   | NOTEOL_UNQUOTED_STRING
-                   | SINGLE_QUOTED_STRING
-                   | DOUBLE_QUOTED_STRING"""
-    p[0] = p[1]
+#def p_char_string(p):
+    #"""char_string : EOL_UNQUOTED_STRING
+                   #| NOTEOL_UNQUOTED_STRING
+                   #| SINGLE_QUOTED_STRING
+                   #| DOUBLE_QUOTED_STRING"""
+    #p[0] = p[1]
 
 #<Number>  :    {<Integer> | <Float> }
 #<Numeric>  :   { <Number> | <Number> '(' <UnsignedInteger> ')' }
-def p_number(p):
-    """number : INTEGER
-              | FLOAT"""
-    p[0] = p[1]
+#def p_number(p):
+    #"""number : INTEGER
+              #| FLOAT"""
+    #p[0] = p[1]
     
 #<TextField>  :     { <SemiColonTextField> }
-def p_text_field(p):
-    """text_field : SEMI_TEXT_FIELD"""
-    p[0] = p[1]
+#def p_text_field(p):
+    #"""text_field : SEMI_TEXT_FIELD"""
+    #p[0] = p[1]
 #<CIF>  :   <Comments>? <WhiteSpace>? { <DataBlock> { <WhiteSpace> <DataBlock> }* { <WhiteSpace> }? }?
 
 #<DataBlock>  :     <DataBlockHeading> {<WhiteSpace> { <DataItems> | <SaveFrame>} }*
@@ -101,8 +105,11 @@ def p_text_field(p):
 #<LoopBody>  :  <Value> { <WhiteSpace> <Value> }*
 
 def p_error(p):
-    sys.stderr.write("Syntax error\n")
+    sys.stderr.write("Syntax error in '%s'\n" % p )
 
+precedence = (
+    ('right', "TAG", "VALUE"),
+)
 
 parser = yacc.yacc()
 
