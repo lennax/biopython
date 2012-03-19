dc7f51dbe1abae07a5a5ccf8fb4881fe2fc3ef94
Lenna Peterson <lxpeters@purdue.edu>
2012-03-19 15:46:45 -0400

    Multiple improvements, still no 30k.

--- Bio/PDB/mmCIF/CIFyacc2.py
+++ Bio/PDB/mmCIF/CIFyacc2.py
@@ -13,14 +13,19 @@ tokens = m.tokens
 
 yacc_start = time.clock()
 
+# Parse entire file into data_pairs
 def p_data_pairs(p):
     """data_pairs : data_pairs data_pair"""
     p[0] = tuple(p[1]) + (p[2],)
+    #print p[2]
 
-def p_data_pairs_empty(p):
-    """data_pairs : empty"""
-    p[0] = ()
+# 1-member data_pairs breaks recursion
+def p_data_pairs_first(p):
+    """data_pairs : data_pair"""
+    p[0] = (p[1],)
+    #print p[1]
 
+# Types of data_pair
 def p_data_data(p):
     """data_pair : DATA"""
     p[0] = ("data", p[1])
@@ -29,43 +34,35 @@ def p_data_pair(p):
     """data_pair : TAG VALUE"""
     p[0] = (p[1], p[2])
 
+def p_loop(p):
+    """data_pair : loop_header values"""
+    p[0] = p[1] + (p[2],)
+
+# Recursion for loop
+def p_loop_header(p):
+    """loop_header : LOOP tags"""
+    p[0] = (p[1],) + (p[2],)
+    
 def p_tag_tags(p):
     """tags : tags TAG"""
     p[0] = tuple(p[1]) + (p[2],)
 
-def p_tags_empty(p):
-    """tags : empty"""
-    p[0] = ()
+def p_tags_first(p):
+    """tags : TAG"""
+    p[0] = (p[1],)
 
 def p_value_values(p):
     """values : values VALUE"""
     p[0] = tuple(p[1]) + (p[2],)
 
-def p_values_empty(p):
-    """values : empty"""
-    p[0] = ()
+def p_values_first(p):
+    """values : VALUE"""
+    p[0] = (p[1],)
 
-def p_loop(p):
-    """data_pair : loop_header values"""
-    p[0] = p[1] + (p[2],)
-
-def p_loop_header(p):
-    """loop_header : LOOP tags"""
-    p[0] = (p[1],) + (p[2],)
-
-def p_empty(p):
-    """empty :"""
-    pass
-    
+# Misc rules
 def p_error(p):
     sys.stderr.write("Syntax error in '%s'\n" % p )
     
-precedence = (
-    # I have no idea how to fix the shift/reduce error
-    # fortunately it is picking the right thing
-    ('right', "TAG"),
-)
-
 parser = yacc.yacc()
 
 if __name__ == "__main__":
@@ -73,9 +70,10 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         
         with open(filename) as fh:
-            result = parser.parse(fh.read(), debug=1)
-            print result
-            yacc_end = time.clock() - yacc_start
-            print "Runtime: ", yacc_end
+            filedump = fh.read()
+        result = parser.parse(filedump, debug=1)
+        print result
+        yacc_end = time.clock() - yacc_start
+        print "Runtime: ", yacc_end
 
 # vim:sw=4:ts=4:expandtab
