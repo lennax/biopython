0c19ce16c0212ed05a7a3cb5ff828db44b0288b7
Lenna Peterson <arklenna@gmail.com>
2012-05-31 15:24:57 -0400

    Basic inserting to sqlite variant db.

--- Bio/Variant/VariantDB.py
+++ Bio/Variant/VariantDB.py
@@ -1,6 +1,7 @@
 
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
+import json
 import sqlite3
 
 
@@ -152,7 +153,7 @@ class VariantSqlite(VariantDB):
         schema['variant'] = """(
             id INTEGER PRIMARY KEY,
             site INTEGER,
-            var_name TEXT,
+            name TEXT,
             ref TEXT,
             alt TEXT,
             misc TEXT,
@@ -162,7 +163,7 @@ class VariantSqlite(VariantDB):
         )"""
 
         for k, v in schema.iteritems():
-            self.cursor.execute('''CREATE TABLE IF NOT EXISTS %s %s''' \
+            self.cursor.execute('''CREATE TABLE IF NOT EXISTS %s %s''' 
                                 % (k, v))
         self.conn.commit()
 
@@ -175,22 +176,52 @@ class VariantSqlite(VariantDB):
 
     def metadata_insert(self, filename, misc):
         """Insert a row into metadata. Return id of inserted row."""
-        insert_string = """INSERT INTO metadata VALUES (
-            NULL, ?, ?, ?, ?)"""
-        self.cursor.execute(insert_string, (filename, misc, 
-                            datetime.now(), datetime.now()))
+        insert_string = "INSERT INTO metadata VALUES (:id, :filename, \
+                        :misc, :create_date, :update_date)"
+        time = self._time()
+        insert_dict = dict(id=None, filename=filename, 
+            misc=json.dumps(misc), create_date=time, update_date=time)
+        self.cursor.execute(insert_string, insert_dict)
         self.conn.commit()
         return self.cursor.lastrowid
 
     def site_insert(self, metadata, accession, position, site_id, misc):
-        pass
+        """Insert a row into site. Return id of inserted row."""
+        insert_string = "INSERT INTO site VALUES (\
+            :id, :metadata, :accession, :position, :site_id, :misc, \
+            :create_date, :update_date)"
+        time = self._time()
+        insert_dict = dict(id=None, metadata=metadata, 
+            accession=accession, position=position, site_id=site_id,
+            misc=json.dumps(misc), create_date=time, update_date=time)
+        self.cursor.execute(insert_string, insert_dict)
+        self.conn.commit()
+        return self.cursor.lastrowid
 
     def variant_insert(self, site, name, ref, alt, misc):
-        pass
+        """Insert a row into variant. Return id of inserted row."""
+        insert_string = "INSERT INTO variant VALUES (:id, :site, \
+            :name, :ref, :alt, :misc, :create_date, :update_date)"
+        time = self._time()
+        insert_dict = dict(id=None, site=site, name=name, 
+            ref=ref, alt=alt, misc=json.dumps(misc), 
+            create_date=time, update_date=time)
+        self.cursor.execute(insert_string, insert_dict)
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def _time(self):
+        "Return current time as YYYY-mm-DD HH:MM:SS"
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     def query(self, query):
         pass
     
 if __name__ == "__main__":
     db = VariantSqlite("test.db")
-
+    meta_row = db.metadata_insert(filename="myfile", misc=dict(FORMATS="blahblah", INFOS="bloobloo"))
+    site_row = db.site_insert(metadata=meta_row, accession="rf8320d", position=12324, site_id="ggsgdg", misc=dict(CHROM=2))
+    variant_row = db.variant_insert(site=site_row, name="NA001", ref="A", alt="G", misc=dict(called=True, phased=False))
+    print "meta", meta_row
+    print "site", site_row
+    print "variant", variant_row
